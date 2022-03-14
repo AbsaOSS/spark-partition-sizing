2790c218e4b023eab5f1bb697722f6cf683e9753
Adrian Olosutean <adi.olosutean@gmail.com>
2022-03-14 11:45:01 +0100

    #5 merge + extra tests

--- src/test/scala/za/co/absa/spark/partition/sizing/DataFramePartitionerTest.scala
+++ src/test/scala/za/co/absa/spark/partition/sizing/DataFramePartitionerTest.scala
@@ -110,9 +110,11 @@ class DataFramePartitionerTest extends AnyFunSuite with SparkTestBase {
     val result6 = df.repartitionByDesiredSize(fromDataframe4SampleSizer)(min, max)
     val result7 = df.repartitionByDesiredSize(fromDataframe6SampleSizer)(min, max)
     assertResult(1)(result2.rdd.partitions.length)
-    assertResult(1)(result3.rdd.partitions.length)
-    assertResult(18)(result4.rdd.partitions.length)
-    assert(result5.rdd.partitions.length >= 1)
+    assertResult(5)(result3.rdd.partitions.length)
+    assertResult(2)(result4.rdd.partitions.length)
+    assert(result5.rdd.partitions.length >= 3)
+    assert(result6.rdd.partitions.length >= 3)
+    assert(result7.rdd.partitions.length >= 3)
 
     assertThrows[IllegalArgumentException](df.repartitionByDesiredSize(fromSchemaSummariesSizer)(min, max))
   }
